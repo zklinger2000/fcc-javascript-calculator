da5dd74b07b72435e3318bea7c3f6c77a433c2da
Zack Klinger <zklinger@gmail.com>
2016-04-07 15:28:44 -0700

    CHANGE addPush to operandPush to take operand type as string variable

--- app.js
+++ app.js
@@ -19,7 +19,11 @@ angular.module('calcApp', [])
   function numPush(num) {
     if (vm.display.length === 1 && vm.display === '0') {
       vm.display = num.toString();
-      vm.buffer = [vm.display];
+      if (vm.buffer[vm.buffer.length - 1]) {
+        vm.buffer[vm.buffer.length - 1] = vm.display;
+      } else {
+        vm.buffer.push(vm.display);
+      }
     } else if (vm.display === '') {
       vm.display = num.toString();
       vm.buffer.push(vm.display);
@@ -28,19 +32,25 @@ angular.module('calcApp', [])
       vm.buffer = [vm.display];
     } else {
       vm.display += num.toString();
-      vm.buffer[vm.buffer.length - 1] = vm.display;
+      if (vm.buffer[vm.buffer.length - 1]) {
+        vm.buffer[vm.buffer.length - 1] = vm.display;
+      } else {
+        vm.buffer.push(vm.display);
+      }
     }
   }
-  function addPush() {
+  
+  function operandPush(value) {
     // If the display is empty, do nothing
     if (vm.display === '0' || vm.display === '') return;
     // If the last element in the buffer is the equals sign, reset buffer
     if (vm.buffer[vm.buffer.length - 1] === '=') {
       vm.buffer = [vm.display];
     }
-    vm.buffer.push('+');
+    vm.buffer.push(value);
     vm.display = '';
   }
+  
   function equalsPush() {
     console.log(vm.buffer);
     if (vm.buffer.length % 2 === 0) {
@@ -49,73 +59,63 @@ angular.module('calcApp', [])
     vm.display = eval(vm.buffer.join(''));
     vm.buffer.push('=');
   }
-  
+
   vm.keys = [{
-    name: 'C',
+    value: 'C',
     func: hello
   }, {
-    name: 'CE',
+    value: 'CE',
     func: hello
   }, {
-    name: '+/-',
+    value: '+/-',
     func: hello
   }, {
-    name: 'X',
-    func: hello
+    value: '*',
+    func: operandPush,
   }, {
-    name: '7',
     func: numPush,
     value: 7
   }, {
-    name: '8',
     func: numPush,
     value: 8
   }, {
-    name: '9',
     func: numPush,
     value: 9
   }, {
-    name: '/',
-    func: hello
+    value: '/',
+    func: operandPush,
   }, {
-    name: '4',
     func: numPush,
     value: 4
   }, {
-    name: '5',
     func: numPush,
     value: 5
   }, {
-    name: '6',
     func: numPush,
     value: 6
   }, {
-    name: '-',
-    func: hello
+    value: '-',
+    func: operandPush,
   }, {
-    name: '1',
     func: numPush,
     value: 1
   }, {
-    name: '2',
     func: numPush,
     value: 2
   }, {
-    name: '3',
     func: numPush,
     value: 3
   }, {
-    name: '+',
-    func: addPush
+    func: operandPush,
+    value: '+'
   }, {
-    name: '0',
     func: numPush,
     value: 0
   }, {
-    name: '.',
+    value: '.',
     func: hello
   }, {
-    name: '=',
+    value: '=',
     func: equalsPush
   }];
 
